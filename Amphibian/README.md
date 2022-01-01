2d1ab741e08c5f147dc79528062c81355427d7a6
sanosuke009@gmail.com <Dell@Dell-PC>
2022-01-01 22:21:42 +0530

    Started adding HttpClient libraries
    Change-Id: I6bf7c18ace5489611657cc8bc15db4dbbb00f680
    Change-Id: A002
    Signed-off-by: sanosuke009@gmail.com <Dell@Dell-PC>

--- Amphibian/README.md
+++ Amphibian/README.md
@@ -1,2 +1,4 @@
 # AmphibianJava
 This repository will hold the Test Automation Framework combining Cucumber, TestNG, Selenium &amp; Appium using Java which can be used for BDD &amp; TDD both.
+
+1. Adding API Test Methods using Apache HttpClient
--- Amphibian/src/test/java/BaseClasses/BaseC.java
+++ Amphibian/src/test/java/BaseClasses/BaseC.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.Scenario;
@@ -16,6 +17,7 @@ public class BaseC {
 	protected Scenario scenario;
 	protected RemoteWebDriver driver;
 	protected WebDriverWait wait;
+	protected FluentWait<RemoteWebDriver> fwait;
 	protected DesiredCapabilities dc;
 	protected String defaultWindow;
 	protected String currentWindow;
